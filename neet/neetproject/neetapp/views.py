706eaf3e91093c5007458b939c32c5b10e894f60
yewonahn <yewonahn@naver.com>
2021-07-24 15:18:17 +0900

    init

---
+++ neet/neetproject/neetapp/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+
+# Create your views here.
+def competition (request):
+    return render (request, "competition.html")
+
+def detailpage (request):
+    return render (request, "detailpage.html")
+
+def home_loginO (request):
+    return render (request, "home_loginO.html")
+
+def home (request):
+    return render (request, "home.html")
+
+def login (request):
+    return render (request, "login.html")
+
+def members (request):
+    return render (request, "members.html")
+
+def myteam (request):
+    return render (request, "myteam.html")
+
+def portfolioh (request):
+    return render (request, "portfolioh.html")
+
+def profile (request):
+    return render (request, "profile.html")
+
+def recruitment (request):
+    return render (request, "recruitment.html")
+
+def recruitmentlist (request):
+    return render (request, "recruitmentlist.html")
+
+def review (request):
+    return render (request, "review.html")
+
+def signup (request):
+    return render (request, "signup.html")
+
+def message (request):
+    return render(request, "message.html")
---
+++ neet/neetproject/neetproject/urls.py
@@ -0,0 +1,37 @@
+"""neetproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+import neetapp.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('competition/',neetapp.views.competition, name="competition"),
+    path('detailpage/',neetapp.views.detailpage, name="detailpage"),
+    path('home_loginO/',neetapp.views.home_loginO, name="home_loginO"),
+    path('home/',neetapp.views.home, name="home"),
+    path('login/',neetapp.views.login, name="login"),
+    path('members/',neetapp.views.members, name="members"),
+    path('myteam/',neetapp.views.myteam, name="myteam"),
+    path('portfolioh/',neetapp.views.portfolioh, name="portfolioh"),
+    path('profile/',neetapp.views.profile, name="profile"),
+    path('recruitment/',neetapp.views.recruitment, name="recruitment"),
+    path('recruitmentlist/',neetapp.views.recruitmentlist, name="recruitmentlist"),
+    path('review/',neetapp.views.review, name="review"),
+    path('signup/',neetapp.views.signup, name="signup"),
+    path('message/',neetapp.views.message,name="message"),
+    path('', neetapp.views.home, name="home"),
+]
