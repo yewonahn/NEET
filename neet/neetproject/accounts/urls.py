137983c13234af09d47a133a20b7f8ae2d912571
yxram <rxcord@naver.com>
2021-08-11 17:58:20 +0900

    0811 17:58

--- neet/neetproject/accounts/urls.py
+++ neet/neetproject/accounts/urls.py
@@ -1,3 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+
+
+urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+]
+
+
+"""
 from django.urls import path
 from . import views
 
@@ -6,3 +19,4 @@ urlpatterns = [
     path('login/', views.login, name ='login'),
     path('logout/', views.logout, name ='logout'),
 ]
+"""
--- neet/neetproject/accounts/views.py
+++ neet/neetproject/accounts/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+"""
 from django.contrib.auth.models import User
 from django.contrib import auth
+"""
 
 # Create your views here.
+def login_view (request) :
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(
+                request=request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+        return render (request, 'login.html', {'form':form})
+
+
+def logout_view (request) :
+    logout(request)
+    return redirect("home")
+
+
+
+"""
 def signup(request):
     if request.method == 'POST':
         if request.POST['password1'] == request.POST['password2']:
@@ -32,4 +59,4 @@ def logout(request):
         redirect('home')
     return render(request,'login.html')
 
-    
+ """   
