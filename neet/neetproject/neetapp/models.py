eb268b17b97adcf0b092acf9e114a2c9caa68ccb
yewonahn <yewonahn@naver.com>
2021-08-11 20:45:47 +0900

    feature/makeTeam

--- neet/neetproject/neetapp/models.py
+++ neet/neetproject/neetapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class MakeTeam(models.Model):
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    content = models.TextField()
+    num = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.content[:80]
